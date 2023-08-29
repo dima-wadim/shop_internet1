5ade20069d1b26c74b271377b689a4902786e051
Вадим <kantora_70@mail.ru>
2023-08-29 17:28:51 +0500

    Create README.md

---
+++ catalog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+#from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'catalog/control_1.html')
+
+def control_2(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        ph_number = request.POST.get("phone")
+        message = request.POST.get("message")
+        print(f'Новое сообщение от  {name}({ph_number}): {message}')
+    return render(request, 'catalog/control_2.html')
