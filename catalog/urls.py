d6a012734e47baad455bbd2fe02f2d3a955276e2
Вадим <kantora_70@mail.ru>
2023-09-02 10:25:56 +0500

    # Conflicts: #	catalog/views.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from catalog.views import index
+from catalog.views import index, contacts
 
-from catalog.views import control_2
+
 
 urlpatterns = [
     path(' ', index),
-    path('catalog/', control_2)
+    path('contacts', contacts)
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'catalog/control_1.html')
 
-def catalog(request):
+def contacts(request):
     if request.method == "POST":
         name = request.POST.get("name")
         ph_number = request.POST.get("phone")
