c3adaac901162845e44bc6b18203f42b74a93ca1
Вадим <kantora_70@mail.ru>
2023-09-02 11:37:40 +0500

    # Conflicts: #	catalog/views.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from catalog.models import Product, Category, Contacts
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'product_name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('product_name', 'descriptions',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'category_name',)
+
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ('contact_name', 'ph_number', 'message',)
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
+import json
+from catalog.models import Product, Contacts
 
-#from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'catalog/control_1.html')
+def home(request):
+    last_products = Product.objects.order_by('-date_create')[:5]
+    for product in last_products:
+        print(product.product_name)
+    return render(request, 'catalog/home.html')
 
 def contacts(request):
+    contact_list = []
     if request.method == "POST":
         name = request.POST.get("name")
         ph_number = request.POST.get("phone")
         message = request.POST.get("message")
-        print(f'Новое сообщение от  {name}({ph_number}): {message}')
-    return render(request, 'catalog/control_2.html')
+        Contacts.objects.create(contact_name=name, ph_number=ph_number, message=message)
+    return render(request, 'catalog/contacts.html')
